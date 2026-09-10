3d18354688cc17151b8a2eb7ed200b200ee0f2d4
Ana Maria <78694472+anamsilva1981@users.noreply.github.com>
2026-09-10 11:41:23 -0300

    fix: restore environment for GitHub Pages build

---
+++ src/environments/environment.ts
@@ -0,0 +1,25 @@
+export const environment = {
+  production: true,
+  mainColor: '#9b202d',
+  url: 'https://anamsilva1981.github.io/nikoly-portifolio/',
+  umamiWebsiteId: '',
+  spotify: {
+    apiURL: 'https://api.spotify.com/v1/',
+    clientId: '',
+    clientSecret: '',
+    clientRefreshToken: ''
+  },
+  traktTv: {
+    url: 'https://trakt.tv/',
+    apiURL: 'https://api.trakt.tv/',
+    version: 2,
+    clientId: '',
+    username: '',
+    token: ''
+  },
+  themoviedb: {
+    apiURL: 'https://api.themoviedb.org/3/',
+    token: '',
+    language: 'en'
+  }
+};
